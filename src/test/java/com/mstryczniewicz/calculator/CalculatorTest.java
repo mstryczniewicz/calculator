c2a8070137cf7cf19ef3264e9080cd0cc015f54f
Marcin Stryczniewicz <mstryczniewicz@gmail.com>
2020-10-09 20:39:44 +0200

    Add calculator unit test

---
+++ src/test/java/com/mstryczniewicz/calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.mstryczniewicz.calculator;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+     private Calculator calculator = new Calculator();
+
+     @Test
+     public void testSum() {
+          assertEquals(6, calculator.sum(2, 3));
+     }
+}
